a8c2e529797bc69327180e990c6ed448cf622086
3sut2z <145888543+3sut2z@users.noreply.github.com>
2024-07-21 11:52:01 +0700

    Update SK_hubv2.lua

--- SK_hubv2.lua
+++ SK_hubv2.lua
@@ -1,36 +1,6 @@
 local player = game.Players.LocalPlayer
 local gui = player:WaitForChild("PlayerGui") -- Get the player's PlayerGui
 
-----------------------------------------------------------------------------------------------------------------------------------------------
-local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/3sut2z/SK-hub/main/Fluent.lua"))()
-local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/3sut2z/SK-hub/main/SaveManager.lua"))()
-local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/3sut2z/SK-hub/main/InterfaceManager.lua"))()
-----------------------------------------------------------------------------------------------------------------------------------------------
-local Window = Fluent:CreateWindow({
-    Title = "SK Hub V2",
-    SubTitle = "SSG,3",
-    TabWidth = 160,
-    Size = UDim2.fromOffset(530, 350),
-    Acrylic = false,
-    Theme = "Dark",
-    MinimizeKey = Enum.KeyCode.End
-})
-local Tabs = {
-    Main = Window:AddTab({ Title = "Main", Icon = "home" }),
-    Setting = Window:AddTab({ Title = "Setting", Icon = "settings" }),
-    Stats = Window:AddTab({ Title = "Stats", Icon = "plus-circle" }),
-    Player = Window:AddTab({ Title = "Player", Icon = "baby" }),
-    Teleport = Window:AddTab({ Title = "Island", Icon = "palmtree" }),
-    Fruit = Window:AddTab({ Title = "Fruit", Icon = "cherry" }),
-    Raid = Window:AddTab({ Title = "Raid", Icon = "swords" }),
-    Race = Window:AddTab({ Title = "Race V4", Icon = "chevrons-right" }),
-    Shop = Window:AddTab({ Title = "Shop", Icon = "shopping-cart" }),
-	Misc = Window:AddTab({ Title = "Misc", Icon = "list-plus" }),
-    Hop = Window:AddTab({ Title = "Hop", Icon = "wifi" }),
-}
-local Options = Fluent.Options
-do
---------------------------------------------------------------------------------------------------------------------------------------------
 --Place Id Check
 if game.PlaceId == 2753915549 then
     World1 = true
